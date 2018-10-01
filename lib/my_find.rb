0a32a009493a9a51dc4b26172688f5e030891385
Victor Gonzalez <mywalletislost@gmail.com>
2018-10-01 22:58:57 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,8 +2,9 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  while i < 100
-    yield(1)
-      i = i + 1
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    end
+    i = i + 1
   end
-end
